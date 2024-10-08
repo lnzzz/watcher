541eabfd1c98b2e5264b19c8f5ad829a260fb6a8
fernando arrigorriaga <picu@MacBook-Pro-3.local>
2024-10-08 10:24:43 -0300

    cron de max views de 7 a 1, fix

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ cron.schedule("0 0,6,12,18 * * *",()=>{
     timezone: "America/Argentina/Buenos_Aires"
 });
 
-cron.schedule('0 7-1 * * 1-5', () => {
+cron.schedule('0 0,1,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * 1-5', () => {
     const dateNow= new Date();
     console.log(`--------******  Cron del twitter ${dateNow}  *****---------`);
     calculateInfoAndTweet(db);
